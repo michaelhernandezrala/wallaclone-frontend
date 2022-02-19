ab475a91278a03edd4d52c9f287514f955e086d0
Michael Hernandez <mhernandezrala@gmail.com>
2022-02-19 12:50:09 +0100

    Esqueleto main

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../Header/Header'
 
-function Layout() {
+function Layout({ title, children }) {
   return (
     <div className='layout'>
       <Header />
+      <main className='layout__main'>
+        <h1 className='layout__main--title'>{title}</h1>
+        <section className='layout__main--content'>{children}</section>
+      </main>
     </div>
   )
 }
