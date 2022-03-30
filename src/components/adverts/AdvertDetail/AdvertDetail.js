a2a7e501ac87c447e4196fa19f77a11ca73613a6
Michael Hernandez <mhernandezrala@gmail.com>
2022-03-30 19:55:38 +0200

    .

--- src/components/adverts/AdvertDetail/AdvertDetail.js
+++ src/components/adverts/AdvertDetail/AdvertDetail.js
@@ -4,6 +4,7 @@ import { deleteAdvert, getAdvert } from '../service';
 import { Button, Photo } from '../../common';
 import Layout from '../../Layout/Layout';
 import { Container } from 'react-bootstrap';
+import './AdvertDetail.css';
 
 function AdvertDetail({ match }) {
   const [advert, setAdvert] = useState();
@@ -31,25 +32,33 @@ function AdvertDetail({ match }) {
     <Layout>
       <section className='AdvertDetail'>
         <Container>
-          <article key={advert.id}>
+          <article key={advert.id} className='AdvertDetail__content'>
             <div className='Photo-div'>
-              <Photo {...photo} />
+              <img src={advert.photo} />
             </div>
             <div className='text-div'>
-              <span className='name'>Name: {advert.name}</span>
+              <span className='name'>
+                <strong>Name:</strong> {advert.name}
+              </span>
               <br />
-              <span className='price'>Price: {advert.price}€</span>
+              <span className='price'>
+                <strong>Price:</strong> {advert.price}€
+              </span>
               <br />
-              <span className='sale'>Sale: {advert.sale ? 'Yes' : 'No'}</span>
+              <span className='sale'>
+                <strong>Sale:</strong> {advert.sale ? 'Yes' : 'No'}
+              </span>
               <br />
-              <span className='tags'>Tags: {advert.tags}</span>
+              <span className='tags'>
+                <strong>Tags:</strong> {advert.tags}
+              </span>
+            </div>
+            <br />
+            <div className='delete-button'>
+              <p>Are you sure you want to delete the advert?</p>
+              <Button onClick={handleDelete}>Delete Advert</Button>
             </div>
           </article>
-          <br />
-          <div className='delete-button'>
-            <p>Are you sure you want to delete the advert?</p>
-            <Button onClick={handleDelete}>Delete Advert</Button>
-          </div>
         </Container>
       </section>
     </Layout>
